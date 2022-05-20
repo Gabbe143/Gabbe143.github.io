de3c8ddf18e2700016864452314d3541d16a5f82
Gabbe143 <72918665+Gabbe143@users.noreply.github.com>
2022-05-20 11:36:16 +0200

    fsfsf

--- Prov.js
+++
@@ -1,8 +0,0 @@
-function getNewPicture() {
-    var Color = document.getElementById("backgroundcolor").value;
-    var writtenText = document.getElementById("inputText").value;
-    var choice = document.getElementById("pictures");
-    var choiceText = choice.options[choice.selectedIndex].text;
-    var div = `<div id="rectangle" style="background-color: ${Color}""><h3>${writtenText}</h3> <img src="Bilder/${choiceText}.png"></img></div>`
-    document.getElementById("showCard").innerHTML = div;
-}
